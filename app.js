1da189615a65b36b478c8652fc4a4a02d2de5659
dahby <dstoll243@gmail.com>
2018-10-25 12:13:19 -0700

    added comments

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const gameSize = document.getElementById('gameSize');
 const boardStart = document.getElementById('gameBoard');
 const selector = document.getElementById('selector');
 
+// Populating drop down for game size
+
 const gameOptions = () => {
   for (let x = minGame; x <= maxGame; x += 2) {
     let num = document.createElement('option');
@@ -19,9 +21,15 @@ gameOptions();
 
 const generateGame = (event) => {
   event.preventDefault();
+
+  // Removing existing game board
+
   while (boardStart.firstChild) {
     boardStart.removeChild(boardStart.firstChild);
   }
+
+  // Dynamically creating the game board
+
   const dimensions = event.target.selector.value;
   for (let i = 0; i < dimensions; i++) {
     let boardRow = document.createElement('section');
@@ -34,5 +42,7 @@ const generateGame = (event) => {
     boardStart.appendChild(boardRow);
   }
 };
+
+// Tying game population to submit button
 
 gameSize.addEventListener('submit', generateGame);
